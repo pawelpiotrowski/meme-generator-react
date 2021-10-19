7763c77d17f4569835c700600a0a5264f13dba05
Pawel Piotrowski <pawel@flytype.org>
2021-10-19 22:30:55 +0100

    refactor store to make better access for testing

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,13 @@
 import { configureStore, Action, ThunkAction } from "@reduxjs/toolkit";
 import rootReducer, { RootState } from "./rootReducer";
 
-const store = configureStore({
-  reducer: rootReducer,
-});
+export function makeStore() {
+  return configureStore({
+    reducer: rootReducer,
+  });
+}
+
+const store = makeStore();
 
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
